0130495a638baa43a06c4e8f05f6ebee2dfee7f9
Kit307 <kintlove555@gmail.com>
2022-02-08 23:30:49 +0700

    first commit

---
+++ src/components/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <h1>product detail</h1>
+    <h1>ID = {{ $route.params.id }}</h1>
+    <h2 class="orange--text">{{ productid }}</h2>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      productid: this.$route.params.id,
+    };
+  },
+};
+</script>
+
+<style></style>
